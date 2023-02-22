b4486deec6a1d89000c87c024540916120685f3a
amirthenew <ah.abshari@hotmail.com>
2023-02-22 14:48:06 +0330

    finished!

---
+++ reset.js
@@ -0,0 +1,16 @@
+let tryUrl = './try.html'
+let cancelBtn = document.querySelector('.cancel')
+let changeBtn = document.querySelector('.change')
+const features = 'scroll=no,width=200,height=150'
+cancelBtn.addEventListener('click',()=>{
+    window.close()
+})
+
+changeBtn.addEventListener('click',()=>{
+    window.open(tryUrl,
+        '_blank',
+        features)
+        window.close()
+})
+
+
